5afde89203664f6460ef11546e4dbd0ad8dd6c82
andrei <andrei.ciocarlie@student.upt.ro>
2024-04-05 14:46:50 +0300

    Initialied and basic code for testing purpose

---
+++ project_main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+int main (int argc, char *argv[]){
+    printf ("Ceva\n");
+    return 0;
+}
