50dd73fc9fe5c35505d72224bab2eb314adf13a8
andrei <andrei.ciocarlie@student.upt.ro>
2024-05-10 01:17:09 +0300

    Week 10 almost full

--- check_file.sh
+++ check_file.sh
@@ -4,7 +4,7 @@
 file_path="$1"
 if [ ! -f "$file_path" ]; then
     echo "File does not exist or is not a regular file: $file_path"
-    exit 2;
+    exit 2
 fi
 
 # Get original permissions
@@ -13,21 +13,27 @@ original_permissions=$(stat -c "%a" "$file_path")
 # Change file permissions to allow reading
 chmod +r "$file_path"
 
-# Check for non-ASCII characters
-if grep -qP '[^\x00-\x7F]' "$file_path"; then
-    echo "File contains non-ASCII characters: $file_path"
+# Get the number of lines, words, and characters in the file
+num_lines=$(wc -l < "$file_path")
+num_words=$(wc -w < "$file_path")
+num_chars=$(wc -m < "$file_path")
+
+# Condition 1: Check if the file meets the specified conditions
+if [ "$num_lines" -lt 3 ] && [ "$num_words" -gt 1000 ] && [ "$num_chars" -gt 2000 ]; then
+    # Condition 2: Check for non-ASCII characters or keywords
+    if grep -qP '[^\x00-\x7F]' "$file_path" || grep -qE 'risk|corrupted' "$file_path"; then
+        echo "$file_path"
+        chmod "$original_permissions" "$file_path"
+        exit 1
+    else
+        echo "SAFE"
+        chmod "$original_permissions" "$file_path"
+        exit 0
+    fi
+else
+    echo "SAFE"
     chmod "$original_permissions" "$file_path"
-    exit 1;
+    exit 0
 fi
 
-# Check for keywords "risk" or "corrupted"
-if grep -qE 'risk|corrupted' "$file_path"; then
-    echo "File contains keyword 'risk' or 'corrupted': $file_path"
-    chmod "$original_permissions" "$file_path"
-    exit 1;
-fi
-
-# Change permissions back to original state
-chmod "$original_permissions" "$file_path"
-exit 0;
 
--- project_main.c
+++ project_main.c
@@ -8,7 +8,10 @@
 #include <unistd.h>
 #include <time.h>
 
+int directory_count = 1;
+
 void processDirectory(char *dir_path, char *output_dir, char *isolated_dir, int i) {
+    int corrupted_files_count = 0;
     DIR *dir;
     struct dirent *in;
     struct stat filestat;
@@ -32,7 +35,12 @@ void processDirectory(char *dir_path, char *output_dir, char *isolated_dir, int 
             exit(EXIT_FAILURE);
         }
 
-        if (S_ISREG(filestat.st_mode)) {
+        if (S_ISREG(filestat.st_mode)) { // if the current thing is a file
+            int pipefd[2];
+            if (pipe(pipefd) == -1) {
+            perror("pipe");
+            exit(EXIT_FAILURE);
+            }
             // Construct the output file path
             char output_path[4096];
             sprintf(output_path, "%s/%s", output_dir, in->d_name);
@@ -41,110 +49,137 @@ void processDirectory(char *dir_path, char *output_dir, char *isolated_dir, int 
             sprintf (octal_string, "%o", filestat.st_mode);
             sprintf (octal_string, octal_string+3, strlen (octal_string+3));
 
-            if (strcmp (octal_string, "000") == 0){ //check if the file has no permissions
+            //if (strcmp (octal_string, "000") == 0){ //check if the file has no permissions
                pid_t pid;
                 if ((pid = fork()) < 0) {
                     perror("error");
                     exit(-1);
                 }
-                if (pid == 0){ // child process to analyze the file with no permissions
-                    printf ("File %s has no permissions\n", in->d_name);
+                if (pid == 0){ // GRAND CHILD PROCESS to analyze the file with no permissions
+                    //printf ("File %s has no permissions\n", in->d_name);
+                    close(pipefd[0]); // close read end of the pipe for grand child process since it has no use in here
                     char shell_script[4096];
                     strcpy(shell_script, "/home/andrei/C_Programms/OS_Project/check_file.sh ");
                     strcat(shell_script, path);
                     int exit_status = system (shell_script); // calling the shell script to check if the file is corrupted
+
                     int command_exit_status = WEXITSTATUS(exit_status);
                     if (command_exit_status == 1){ // the file is corrupted
-                        printf ("Fisier corupt \n");
+                        write (pipefd[1], in->d_name, sizeof  (in->d_name) ); //write the name of the file to the pipe
+                    }
+                    else if (command_exit_status == 0){ // the file is not corrupted
+                        write (pipefd[1], "SAFE", sizeof ("SAFE")); //write the SAFE keyword to the pipe
+                    }
+                    else if (command_exit_status == 2){
+                        perror ("File does not exist or is not a regular file\n");
+                        write (pipefd[1], "ERROR", sizeof ("ERROR")); //write the error keyword to the pipe
+                        exit (EXIT_FAILURE);
+                    }
+                    close(pipefd[1]); // close write end of the pipe for the grand child process
+                    exit(0);
+                }
+            //}
+            sleep (1); // child process sleeps for 1 second before creating the next  grand child
+            // CHILD PROCESS
+            // Open the output file
+            close(pipefd[1]); // close write end of the pipe for the child process since it has no use in here
+            char pipe_buffer[1000];
+            if ( read (pipefd[0], pipe_buffer, sizeof (pipe_buffer)) == -1 ){
+                perror("read from pipe error \n");
+                exit(EXIT_FAILURE);
+            }
+            else{
+                close(pipefd[0]); // close read end of the pipe for the child process
+                //printf ("The pipe read this: %s\n", pipe_buffer);
+                if (strcmp (pipe_buffer, "SAFE") == 0){ //if the file is safe
+                    int output;
+                    if ((output = open(output_path, O_CREAT | O_WRONLY | O_TRUNC, S_IWGRP | S_IWUSR | S_IRUSR)) < 0) {
+                        perror("Can't open output file\n");
+                        exit(EXIT_FAILURE);
+                    }
+                    write(output, "Entry: ", strlen("Entry: "));
+
+                    if (write(output, in->d_name, strlen(in->d_name)) == -1) {
+                        perror("Error writing to output file\n");
+                        exit(EXIT_FAILURE);
+                    }
+
+                    write (output, "\nSize : ", strlen("\nSize : ")); //information about size
+                    char str_get_something[100];
+                    sprintf (str_get_something, "%ld", filestat.st_size);
+                    write (output, str_get_something, strlen (str_get_something));
+                    write (output, " bytes", strlen (" bytes"));
+
+                    write (output, "\nInode number :", strlen ("\nInode number :"));// information about inode number
+                    sprintf (str_get_something, "%ld", filestat.st_ino);
+                    write (output, str_get_something, strlen (str_get_something));
+
+                    write (output, "\nPermissions: ", strlen ("\nPermissions: ")); // information about permissions
+                    sprintf (str_get_something, "%o", filestat.st_mode);
+                    sprintf (str_get_something, str_get_something+3, strlen (str_get_something+3));
+                    for (int i = 0 ; i < strlen (str_get_something); i++){
+                        switch (str_get_something[i]) {
+                            case '0':
+                                write (output, "---", 3);
+                                break;
+                            case '1':
+                                write(output, "--x", 3);
+                                break;
+                            case '2':
+                                write(output, "-w-", 3);
+                                break;
+                            case '3':
+                                write(output, "-wx", 3);
+                                break;
+                            case '4':
+                                write(output, "r--", 3);
+                                break;
+                            case '5':
+                                write(output, "r-x", 3);
+                                break;
+                            case '6':
+                                write(output, "rw-", 3);
+                                break;
+                            case '7':
+                                write(output, "rwx", 3);
+                                break;
+                        }
+                    }
+
+                    write(output, "\nLast Modified: ", strlen("\nLast Modified: ")); // Information about last modified time
+                    char *last_modified_time = ctime(&filestat.st_mtime);
+                    write(output, last_modified_time, strlen(last_modified_time));
+
+                    close(output);
+                }
+                else{
+                    if (strcmp (pipe_buffer, "ERROR") == 0){ //if the pipe is not working properly
+                        perror ("Pipe is not working !\n");
+                        exit (EXIT_FAILURE);
+                    }
+                    else{// if the file is corrupted
+                        //printf ("Fisier corupt \n");
+                        corrupted_files_count++;
                         char isolated_file[4096] = "";
                         strcat(isolated_file, isolated_dir);
                         strcat(isolated_file, "/");
                         strcat(isolated_file, in->d_name);
                         if (rename(path, isolated_file) == 0) { // move the file to the isolated directory
-                            printf("File moved successfully.\n");
+                            printf("Corrupted file moved successfully.\n");
                         } else {
                             perror("Error renaming file");
                             exit(EXIT_FAILURE);
                         }
                     }
-                    else if (command_exit_status == 0){ // the file is not corrupted
-                        printf ("Fisierul nu este corupt\n");
-                    }
-                    else if (command_exit_status == 2){
-                        perror ("File does not exist or is not a regular file\n");
-                        exit (EXIT_FAILURE);
-                    }
-                    exit(0);
-                }
-                sleep (1); // Parent process sleeps for 1 second before creating the next child
-            }
-
-            // Open the output file
-            int output;
-            if ((output = open(output_path, O_CREAT | O_WRONLY | O_TRUNC, S_IWGRP | S_IWUSR | S_IRUSR)) < 0) {
-                perror("Can't open output file\n");
-                exit(EXIT_FAILURE);
-            }
-            write(output, "Entry: ", strlen("Entry: "));
-
-            if (write(output, in->d_name, strlen(in->d_name)) == -1) {
-                perror("Error writing to output file\n");
-                exit(EXIT_FAILURE);
-            }
-
-            write (output, "\nSize : ", strlen("\nSize : ")); //information about size
-            char str_get_something[100];
-            sprintf (str_get_something, "%ld", filestat.st_size);
-            write (output, str_get_something, strlen (str_get_something));
-            write (output, " bytes", strlen (" bytes"));
-
-            write (output, "\nInode number :", strlen ("\nInode number :"));// information about inode number
-            sprintf (str_get_something, "%ld", filestat.st_ino);
-            write (output, str_get_something, strlen (str_get_something));
-
-            write (output, "\nPermissions: ", strlen ("\nPermissions: ")); // information about permissions
-            sprintf (str_get_something, "%o", filestat.st_mode);
-            sprintf (str_get_something, str_get_something+3, strlen (str_get_something+3));
-            for (int i = 0 ; i < strlen (str_get_something); i++){
-                switch (str_get_something[i]) {
-                    case '0':
-                        write (output, "---", 3);
-                        break;
-                    case '1':
-                        write(output, "--x", 3);
-                        break;
-                    case '2':
-                        write(output, "-w-", 3);
-                        break;
-                    case '3':
-                        write(output, "-wx", 3);
-                        break;
-                    case '4':
-                        write(output, "r--", 3);
-                        break;
-                    case '5':
-                        write(output, "r-x", 3);
-                        break;
-                    case '6':
-                        write(output, "rw-", 3);
-                        break;
-                    case '7':
-                        write(output, "rwx", 3);
-                        break;
                 }
             }
 
-            write(output, "\nLast Modified: ", strlen("\nLast Modified: ")); // Information about last modified time
-            char *last_modified_time = ctime(&filestat.st_mtime);
-            write(output, last_modified_time, strlen(last_modified_time));
-
-            close(output);
-        } else if (S_ISDIR(filestat.st_mode)) {
+        } else if (S_ISDIR(filestat.st_mode)) { //if the current thing is a directory
             processDirectory(path, output_dir, isolated_dir, i);
         }
     }
-
     closedir(dir);
+    
 }
 
 
@@ -193,26 +228,26 @@ int main(int argc, char* argv[]) {
                 perror("error");
                 exit(-1);
             }
-            if (pid == 0){
-                processDirectory(argv[i], output_dir, isolated_dir, i);
+            if (pid == 0){ // child process // the function processDirectory () will execute in the child process
+                processDirectory(argv[i], output_dir, isolated_dir, ++directory_count);
                 printf("Snapshot of directory %s has been created\n", argv[i]);
                 exit(0);
             }
         }
     }
 
+    //PARENT PROCESS
     sleep (1); // Parent process sleeps for 1 second before creating the next child
     pid_t pid_fiu;
     int status;
     for (int i = 0 ; i < argc-5; i++){        
         pid_fiu = wait(&status);
         if (WIFEXITED (status) > 0){
-            printf("Child process %d terminated with pid %d and exit code %d\n", i+1, pid_fiu, WEXITSTATUS(status));
+            printf("Child process %d terminated with pid %d, exit code %d and x files with potential danger.\n", i+1, pid_fiu, WEXITSTATUS(status) );
         }
     }
 
     free (output_dir);
-    
     return 0;
 }
 
